19ac1215bb6e75fd7de0942e0758d85f39d4be62
sunnydas <das.sunny@gmail.com>
2022-02-14 21:53:21 +0100

    Reverse integer.

--- src/main/java/ReverseInteger.java
+++ src/main/java/ReverseInteger.java
@@ -1,6 +1,39 @@
 
 public class ReverseInteger {
 
+
+    public static int reverseEditorial(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            } else if (rev < Integer.MIN_VALUE || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            rev = 10 * rev + pop;
+        }
+        return rev;
+    }
+
+    public static int reverseAlt(int x) {
+        int i = Math.abs(x);
+        StringBuilder reversed = new StringBuilder();
+        if (x < 0) {
+            reversed.append("-");
+        }
+        while (i > 0) {
+            reversed.append(i % 10);
+            i = i / 10;
+        }
+        try {
+            return Integer.parseInt(reversed.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
     public static int reverse(int x) {
         char[] rep = String.valueOf(x).toCharArray();
         int i = 0;
@@ -24,7 +57,7 @@ public class ReverseInteger {
                 return 0;
             }
             return value;
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             return 0;
         }
     }
@@ -34,6 +67,8 @@ public class ReverseInteger {
         System.out.println(reverse(-123));
         System.out.println(reverse(120));
         System.out.println(reverse(Integer.MAX_VALUE));
+        System.out.println(reverseAlt(-123));
+        System.out.println(reverseEditorial(-123));
     }
 
 }
