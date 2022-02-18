7b2ea51eb02b33ce7d537ca52f8975424aa38d3a
sunnydas <das.sunny@gmail.com>
2022-02-18 19:07:55 +0100

    Non working solution.

--- src/main/java/RegularExpressionMatching.java
+++ src/main/java/RegularExpressionMatching.java
@@ -7,10 +7,74 @@ public class RegularExpressionMatching {
         return pattern.matcher(s).matches();
     }
 
+    public static boolean isMatchAlt(String s, String p) {
+        if (!p.contains(".") && !p.contains("*")) {
+            return p.equals(s);
+        }
+        int i = 0;
+        int j = 0;
+        //"aabcd"
+        // ".*e.*"
+        Character previousCharForWildcard = null;
+        while (i < s.length() && j < p.length()) {
+            char currentChar = s.charAt(i);
+            char currentPattern = p.charAt(j);
+            if ((currentPattern == '*') && (previousCharForWildcard == null || currentChar == previousCharForWildcard)) {
+                i++;
+                previousCharForWildcard = currentChar;
+            } else if (currentPattern == '.') {
+                previousCharForWildcard = currentChar;
+                i++;
+                j++;
+            } else if (currentChar == currentPattern) {
+                i++;
+                j++;
+                previousCharForWildcard = currentChar;
+            } else if (currentChar != currentPattern) {
+                if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                    j = j + 2;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println(i + " " + j);
+        if (i < s.length() - 1 || j < p.length() - 1) {
+            if (j < p.length() - 1) {
+                char x = p.charAt(p.length() - 1);
+                if (x == s.charAt(s.length() - 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isMatch("aa", "a"));
+       /* System.out.println(isMatch("aa", "a"));
+        System.out.println(isMatchAlt("aa", "a"));
         System.out.println(isMatch("aa", "aa"));
+        System.out.println(isMatchAlt("aa", "aa"));
         System.out.println(isMatch("aa", "a*"));
+        System.out.println(isMatchAlt("aa", "a*"));
         System.out.println(isMatch("ab", ".*"));
+        System.out.println(isMatchAlt("ab", ".*"));
+        System.out.println(isMatch("aab", "c*a*b"));
+        System.out.println(isMatchAlt("aab", "c*a*b"));
+        System.out.println(isMatch("ab", ".*c"));
+        System.out.println(isMatchAlt("ab", ".*c"));
+        System.out.println(isMatch("aab", "c*a*b"));
+        System.out.println(isMatchAlt("aab", "c*a*b"));
+        *//*
+        "mississippi"
+"mis*is*ip*."
+         */
+        System.out.println(isMatch("mississippi", "mis*is*ip*."));
+        System.out.println(isMatchAlt("mississippi", "mis*is*ip*."));
     }
 }
