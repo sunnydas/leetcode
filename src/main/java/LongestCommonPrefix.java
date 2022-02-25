2248178c85fd98f16d7544860a66e9b599177bdc
sunnydas <das.sunny@gmail.com>
2022-02-25 17:50:19 +0100

    Alternate solution.

--- src/main/java/LongestCommonPrefix.java
+++ src/main/java/LongestCommonPrefix.java
@@ -1,4 +1,39 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class LongestCommonPrefix {
+
+
+    public static String longestCommonPrefixAlt(String[] strs) {
+        String longestCommonPrefix = "";
+        if (strs.length > 1) {
+            Map<String, Integer> trackerMap = new HashMap<>();
+            for (int i = 0; i < strs.length; i++) {
+                String currentStr = strs[i];
+                int end = currentStr.length() - 1;
+                while (end >= 0) {
+                    String prefix = currentStr.substring(0, end + 1);
+                    if (trackerMap.containsKey(prefix)) {
+                        trackerMap.put(prefix, trackerMap.get(prefix) + 1);
+                    } else {
+                        trackerMap.put(prefix, 1);
+                    }
+                    end--;
+                }
+            }
+            Iterator<Map.Entry<String, Integer>> iterator = trackerMap.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Integer> entry = iterator.next();
+                if (entry.getValue() == strs.length && entry.getKey().length() > longestCommonPrefix.length()) {
+                    longestCommonPrefix = entry.getKey();
+                }
+            }
+        } else if (strs.length == 1) {
+            longestCommonPrefix = strs[0];
+        }
+        return longestCommonPrefix;
+    }
 
 
     public static String longestCommonPrefix(String[] strs) {
@@ -32,7 +67,12 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         String[] strs = new String[]{"flower", "flow", "flight"};
         System.out.println(longestCommonPrefix(strs));
+        System.out.println(longestCommonPrefixAlt(strs));
         strs = new String[]{"dog", "racecar", "car"};
         System.out.println(longestCommonPrefix(strs));
+        System.out.println(longestCommonPrefixAlt(strs));
+        strs = new String[]{"ab", "a"};
+        System.out.println(longestCommonPrefix(strs));
+        System.out.println(longestCommonPrefixAlt(strs));
     }
 }
